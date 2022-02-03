1d6ff37b1a06b431ed4c9d2a5d6ee16c9bdb54c6
bombcar <github@bombcar.com>
2022-02-03 17:27:00 -0600

    update build

--- src/main/kotlin/net/shadowfacts/forgelin/Forgelin.kt
+++ src/main/kotlin/net/shadowfacts/forgelin/Forgelin.kt
@@ -8,12 +8,11 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent
 /**
  * @author shadowfacts
  */
-@Mod(modid = Forgelin.MOD_ID, name = Forgelin.NAME, version = Forgelin.VERSION, acceptableRemoteVersions = "*", acceptedMinecraftVersions = "*", modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter")
+@Mod(modid = Forgelin.MOD_ID, name = Forgelin.NAME, version = "GRADLETOKEN_VERSION", acceptableRemoteVersions = "*", acceptedMinecraftVersions = "*", modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter")
 object Forgelin {
 
 	const val MOD_ID = "forgelin"
 	const val NAME = "Forgelin"
-	const val VERSION = "1.9.2-GTNH-1.7.10-Edition"
 
 	@Mod.EventHandler
 	fun onPreInit(event: FMLPreInitializationEvent) {
