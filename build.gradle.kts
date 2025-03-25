1350896a75dc4c5987011468d4ee5a0789eb98e4
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-03-25 07:49:29 -0400

    Shade source jars

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,9 @@ plugins {
     id("com.gtnewhorizons.gtnhconvention")
     id("org.jetbrains.kotlin.jvm")
 }
+
+// Shadow source jars too
+val shadowSources = configurations.getByName("shadowSources")
+tasks.sourcesJar {
+    from(shadowSources.map { zipTree(it) })
+}
