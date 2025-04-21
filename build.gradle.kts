fcb21ab49c90781d08a2af428d688549e9995521
Maya <10861407+serenibyss@users.noreply.github.com>
2025-04-21 01:39:42 -0500

    fix (?)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,6 @@ plugins {
 // Shadow source jars too
 val shadowSources = configurations.getByName("shadowSources")
 tasks.sourcesJar {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     from(shadowSources.map { zipTree(it) })
 }
