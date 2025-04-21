8fb24b87d3289d6e7f60fb078612c9706b0101fa
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-04-21 02:57:41 -0400

    Shade Kotlin sources (#12)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,10 @@ plugins {
     id("com.gtnewhorizons.gtnhconvention")
     id("org.jetbrains.kotlin.jvm")
 }
+
+// Shadow source jars too
+val shadowSources = configurations.getByName("shadowSources")
+tasks.sourcesJar {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(shadowSources.map { zipTree(it) })
+}
